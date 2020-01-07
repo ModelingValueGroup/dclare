bf6708f4a37e064d2b00edda2d8b4f9dd2f9d4a1
Tom Brus <tombrus@gmail.com>
2020-01-07 23:51:08 +0100

    cosmetic

--- project.sh
+++ project.sh
@@ -24,10 +24,10 @@
 #       t = test
 #===================================================================
 artifacts=(
-  "org.modelingvalue   dclare                  0.0.6       jar jds"
+  "org.modelingvalue   dclare                  0.0.7       jar jds"
 )
 dependencies=(
-  "org.modelingvalue   immutable-collections   1.0.15      jar jds-"
+  "org.modelingvalue   immutable-collections   1.0.16      jar jds-"
 
   "junit               junit                   4.12        jar jdst"
   "org.hamcrest        hamcrest-core           1.3         jar jdst"
