69e4f19f190ed032018651562d192780ce8f2f76
Tom Brus <tombrus@gmail.com>
2020-12-17 12:41:04 +0100

    prepare for release

--- project.sh
+++ project.sh
@@ -13,7 +13,7 @@
 ##     Arjan Kok, Carel Bast                                                                                           ~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-version="1.4.0"
+version="1.5.0"
 artifacts=(
   "org.modelingvalue   dclare                  $version    jar jds"
 )
