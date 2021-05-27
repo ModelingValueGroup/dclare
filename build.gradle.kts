9503e717fc0151519cfa79875fdeb6233c6f3276
Tom Brus <tombrus@gmail.com>
2021-05-27 23:39:17 +0200

    merge changes from develop

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,9 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "0.4.26"
+    id("org.modelingvalue.gradle.mvgplugin") version "0.4.29"
 }
 dependencies {
-    
-    
-    //testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("org.modelingvalue:immutable-collections:1.5.0-BRANCHED")
     implementation("org.modelingvalue:mvgjson:1.1.6-BRANCHED")
 }
