b459c355928411420e6cd82941ecf7c98764433f
Tom Brus <tombrus@gmail.com>
2022-02-10 15:32:59 +0100

    bump gradle version and adapt to newer json library

--- src/main/java/org/modelingvalue/dclare/sync/JsonIC.java
+++ src/main/java/org/modelingvalue/dclare/sync/JsonIC.java
@@ -15,15 +15,17 @@
 
 package org.modelingvalue.dclare.sync;
 
+import org.modelingvalue.collections.List;
+import org.modelingvalue.collections.Map;
+import org.modelingvalue.json.FromJsonBase;
+import org.modelingvalue.json.Json;
+import org.modelingvalue.json.ToJson;
+
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.stream.Stream;
-
-import org.modelingvalue.collections.List;
-import org.modelingvalue.collections.Map;
-import org.modelingvalue.json.*;
 
 public class JsonIC extends Json {
     public static String toJson(Object o) {
@@ -85,8 +87,8 @@ public class JsonIC extends Json {
         }
 
         @Override
-        protected Map<String, Object> makeMapEntry(Map<String, Object> m, String key, Object value) {
-            return m == null ? null : m.put(key, value);
+        protected Map<String, Object> makeMapEntry(Map<String, Object> m, Object key, Object value) {
+            return m == null ? null : m.put(key == null ? null : key.toString(), value);
         }
 
         @Override
