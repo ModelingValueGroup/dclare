867c937b2e448f6f7c110a81b94462e83f72e0c0
WimBast <wim.bast@gmail.com>
2022-06-23 13:44:20 +0200

    construct only in non-readonly transaction

--- src/main/java/org/modelingvalue/dclare/LeafTransaction.java
+++ src/main/java/org/modelingvalue/dclare/LeafTransaction.java
@@ -143,9 +143,7 @@ public abstract class LeafTransaction extends Transaction {
     @SuppressWarnings("unchecked")
     public <O extends Newable> O construct(Construction.Reason reason, Supplier<O> supplier) {
         Newable result = constantState().get(this, reason, Construction.CONSTRUCTED, c -> supplier.get());
-        if (!(LeafTransaction.getCurrent() instanceof ReadOnlyTransaction) || LeafTransaction.getCurrent() instanceof DerivationTransaction) {
-            Newable.D_DIRECT_CONSTRUCTION.set(result, Construction.of(reason));
-        }
+        Newable.D_DIRECT_CONSTRUCTION.set(result, Construction.of(reason));
         return (O) result;
     }
 
