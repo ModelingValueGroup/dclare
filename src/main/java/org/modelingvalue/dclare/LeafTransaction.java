2746454a0577210e1d72ed862ee55fe8ff777c62
WimBast <wim.bast@gmail.com>
2022-08-22 08:43:47 +0200

    support for null result construct

--- src/main/java/org/modelingvalue/dclare/LeafTransaction.java
+++ src/main/java/org/modelingvalue/dclare/LeafTransaction.java
@@ -143,7 +143,7 @@ public abstract class LeafTransaction extends Transaction {
     @SuppressWarnings("unchecked")
     public <O extends Newable> O construct(Construction.Reason reason, Supplier<O> supplier) {
         Newable result = constantState().get(this, reason, Construction.CONSTRUCTED, c -> supplier.get());
-        if (!(LeafTransaction.getCurrent() instanceof ReadOnlyTransaction)) {
+        if (result != null && !(LeafTransaction.getCurrent() instanceof ReadOnlyTransaction)) {
             Newable.D_DIRECT_CONSTRUCTION.set(result, Construction.of(reason));
         }
         return (O) result;
