6cab7a72ef47a3b46f7bdb9010010e1c72e4a72a
WimBast <wim.bast@gmail.com>
2022-10-11 11:22:48 +0200

    make get parentContainer overrideable

--- src/main/java/org/modelingvalue/dclare/Mutable.java
+++ src/main/java/org/modelingvalue/dclare/Mutable.java
@@ -28,27 +28,27 @@ import org.modelingvalue.collections.util.Pair;
 @SuppressWarnings("unused")
 public interface Mutable extends TransactionClass {
 
-    Mutable                                               THIS                     = new This();
+    Mutable                                      THIS                     = new This();
 
-    Set<Mutable>                                          THIS_SINGLETON           = Set.of(THIS);
+    Set<Mutable>                                 THIS_SINGLETON           = Set.of(THIS);
 
     @SuppressWarnings("rawtypes")
-    Observed<Mutable, Pair<Mutable, Setable<Mutable, ?>>> D_PARENT_CONTAINING      = Observed.of("D_PARENT_CONTAINING", null, plumbing, preserved);
+    ParentContaining                             D_PARENT_CONTAINING      = new ParentContaining("D_PARENT_CONTAINING", null, plumbing, preserved);
 
-    Setable<Mutable, TransactionId>                       D_CHANGE_ID              = Setable.of("D_CHANGE_ID", null, plumbing);
+    Setable<Mutable, TransactionId>              D_CHANGE_ID              = Setable.of("D_CHANGE_ID", null, plumbing);
 
     @SuppressWarnings({"rawtypes", "unchecked"})
-    Setable<Mutable, Set<? extends Observer<?>>>          D_OBSERVERS              = Setable.of("D_OBSERVERS", Set.of(), (tx, obj, pre, post) -> Setable.<Set<? extends Observer<?>>, Observer> diff(pre, post,              //
-            added -> added.trigger(obj),                                                                                                                                                                                     //
+    Setable<Mutable, Set<? extends Observer<?>>> D_OBSERVERS              = Setable.of("D_OBSERVERS", Set.of(), (tx, obj, pre, post) -> Setable.<Set<? extends Observer<?>>, Observer> diff(pre, post,              //
+            added -> added.trigger(obj),                                                                                                                                                                            //
             removed -> removed.deObserve(obj)));
 
-    Observer<Mutable>                                     D_OBSERVERS_RULE         = NonCheckingObserver.of("D_OBSERVERS_RULE", m -> D_OBSERVERS.set(m, m.dAllObservers().toSet()));
+    Observer<Mutable>                            D_OBSERVERS_RULE         = NonCheckingObserver.of("D_OBSERVERS_RULE", m -> D_OBSERVERS.set(m, m.dAllObservers().toSet()));
 
     @SuppressWarnings("unchecked")
-    Observer<Mutable>                                     D_PUSHING_CONSTANTS_RULE = NonCheckingObserver.of("D_PUSHING_CONSTANTS_RULE", m -> MutableClass.D_PUSHING_CONSTANTS.get(m.dClass()).forEachOrdered(c -> c.get(m)));
+    Observer<Mutable>                            D_PUSHING_CONSTANTS_RULE = NonCheckingObserver.of("D_PUSHING_CONSTANTS_RULE", m -> MutableClass.D_PUSHING_CONSTANTS.get(m.dClass()).forEachOrdered(c -> c.get(m)));
 
     default Pair<Mutable, Setable<Mutable, ?>> dParentContaining() {
-        return D_PARENT_CONTAINING.get(this);
+        return D_PARENT_CONTAINING.superGet(this);
     }
 
     default Mutable dParent() {
@@ -198,6 +198,23 @@ public interface Mutable extends TransactionClass {
 
     default ConstantState dMemoization(AbstractDerivationTransaction tx) {
         return tx.memoization();
+    }
+
+    static class ParentContaining extends Observed<Mutable, Pair<Mutable, Setable<Mutable, ?>>> {
+
+        private ParentContaining(Object id, Pair<Mutable, Setable<Mutable, ?>> def, SetableModifier... modifiers) {
+            super(id, def, null, null, null, modifiers);
+        }
+
+        @Override
+        public Pair<Mutable, Setable<Mutable, ?>> get(Mutable object) {
+            return object.dParentContaining();
+        }
+
+        private Pair<Mutable, Setable<Mutable, ?>> superGet(Mutable object) {
+            return super.get(object);
+        }
+
     }
 
 }
