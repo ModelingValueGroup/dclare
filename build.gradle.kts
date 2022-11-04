6957e9cfa34e99c75c3710d1d296c9801c0264ce
Tom Brus <tombrus@gmail.com>
2022-11-04 23:20:25 +0100

    prepare for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ plugins {
     eclipse
 }
 dependencies {
-    implementation("org.modelingvalue:immutable-collections:2.1.0-BRANCHED")
-    implementation("org.modelingvalue:mvg-json:2.1.0-BRANCHED")
+    implementation("org.modelingvalue:immutable-collections:2.1.1-BRANCHED")
+    implementation("org.modelingvalue:mvg-json:2.1.1-BRANCHED")
 }
 publishing {
     publications {
