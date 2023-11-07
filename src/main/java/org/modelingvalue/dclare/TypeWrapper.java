885f76b2296cce75fa29391bb4b3d9d77b6a3ab0
WimBast <wim.bast@gmail.com>
2023-11-07 10:29:28 +0100

    toList -> asList

--- src/main/java/org/modelingvalue/dclare/TypeWrapper.java
+++ src/main/java/org/modelingvalue/dclare/TypeWrapper.java
@@ -15,72 +15,84 @@
 
 package org.modelingvalue.dclare;
 
+import java.util.Optional;
+
 import org.modelingvalue.collections.List;
 import org.modelingvalue.collections.util.IdentifiedBy;
 import org.modelingvalue.collections.util.Pair;
 
-import java.util.Optional;
-
 public interface TypeWrapper {
 
     Constant<TypeWrapper, List<TypeWrapper>> SUPERS = Constant.of(Pair.of("SUPERS", TypeWrapper.class), TypeWrapper::computeSupers);
+
     List<TypeWrapper> computeSupers();
 
-    default List<TypeWrapper> supers(){
+    default List<TypeWrapper> supers() {
         return SUPERS.get(this);
     }
 
     Constant<Pair<TypeWrapper, TypeWrapper>, Boolean> IS_ASSIGNABLE_TO = Constant.of(Pair.of("IS_ASSIGNABLE_TO", TypeWrapper.class), TypeWrapper::isAssignableTo);
-    private static boolean isAssignableTo(Pair<TypeWrapper, TypeWrapper> pair){
+
+    private static boolean isAssignableTo(Pair<TypeWrapper, TypeWrapper> pair) {
         var sub = pair.a();
         var sup = pair.b();
         return sub.equals(sup) || sub.supers().anyMatch(t -> IS_ASSIGNABLE_TO.get(Pair.of(t, sup)));
     }
 
-    default boolean isAssignableTo(TypeWrapper type){
+    default boolean isAssignableTo(TypeWrapper type) {
         return IS_ASSIGNABLE_TO.get(Pair.of(this, type));
     }
 
-    default boolean isSuperOf(TypeWrapper sub){
-        if(this.equals(sub)) return false;
-        return sub.isAssignableTo(this);
+    default boolean isSuperOf(TypeWrapper sub) {
+        if (this.equals(sub)) {
+            return false;
+        } else {
+            return sub.isAssignableTo(this);
+        }
     }
 
-    default boolean isSubOf(TypeWrapper sup){
-        if(this.equals(sup)) return false;
-        return this.isAssignableTo(sup);
+    default boolean isSubOf(TypeWrapper sup) {
+        if (this.equals(sup)) {
+            return false;
+        } else {
+            return this.isAssignableTo(sup);
+        }
     }
 
     Constant<TypeWrapper, List<TypeWrapper>> LINEARIZATION = Constant.of(Pair.of("LINEARIZATION", TypeWrapper.class), tw -> TypeWrapper.linearization(tw));
-    private static List<TypeWrapper> linearization(TypeWrapper type){
+
+    private static List<TypeWrapper> linearization(TypeWrapper type) {
         var supers = type.supers();
-        var ls = supers.map(LINEARIZATION::get).toList();
+        var ls = supers.map(LINEARIZATION::get).asList();
         return merge(List.of(type), ls, supers);
     }
 
-    private static List<TypeWrapper> merge(List<TypeWrapper> result, List<List<TypeWrapper>> supers, List<TypeWrapper> immediates){
-        var supersFiltered = supers.filter(m -> !m.isEmpty()).toList();
-        var combined = supers.add(immediates).filter(m -> !m.isEmpty()).toList();
-        if(combined.isEmpty()) return result;
-        var candidate = getMergeCandidate(combined)
-                .or(() -> getMergeCandidate(supersFiltered))
-                .orElseGet(() -> combined.get(0).get(0));
-        return merge(result.add(candidate), supersFiltered.map(l -> l.remove(candidate)).toList(), immediates.remove(candidate));
+    private static List<TypeWrapper> merge(List<TypeWrapper> result, List<List<TypeWrapper>> supers, List<TypeWrapper> immediates) {
+        var combined = supers.add(immediates).filter(m -> !m.isEmpty()).asList();
+        if (combined.isEmpty()) {
+            return result;
+        } else {
+            var supersFiltered = supers.filter(m -> !m.isEmpty()).asList();
+            TypeWrapper candidate = getMergeCandidate(combined).or(() -> getMergeCandidate(supersFiltered)).orElseGet(() -> combined.get(0).get(0));
+            return merge(result.add(candidate), supersFiltered.map(l -> l.remove(candidate)).asList(), immediates.remove(candidate));
+        }
     }
 
-    private static Optional<TypeWrapper> getMergeCandidate(List<List<TypeWrapper>> merge){
+    private static Optional<TypeWrapper> getMergeCandidate(List<List<TypeWrapper>> merge) {
         for (var list : merge) {
             var head = list.get(0);
-            if(merge.noneMatch(l -> l.lastIndexOf(head) > 0)) return Optional.of(head);
+            if (merge.noneMatch(l -> l.lastIndexOf(head) > 0)) {
+                return Optional.of(head);
+            }
         }
         return Optional.empty();
     }
 
-    default List<TypeWrapper> linearization(){
+    default List<TypeWrapper> linearization() {
         return LINEARIZATION.get(this);
     }
 
-    class ObjectType extends IdentifiedBy<Class<?>> implements TypeWrapper{
+    class ObjectType extends IdentifiedBy<Class<?>> implements TypeWrapper {
 
         public ObjectType(Class<?> clazz) {
             super(clazz);
@@ -91,9 +103,8 @@ public interface TypeWrapper {
             if (this.id() == Object.class) {
                 return List.of();
             } else {
-                var sups = List.<Class<?>, TypeWrapper>of(ObjectType::new, this.id().getInterfaces());
-                sups = sups.add(this.id().getSuperclass() == null ? new ObjectType(Object.class) : new ObjectType(this.id().getSuperclass()));
-                return sups;
+                var sups = List.<Class<?>, TypeWrapper> of(ObjectType::new, this.id().getInterfaces());
+                return sups.add(this.id().getSuperclass() == null ? new ObjectType(Object.class) : new ObjectType(this.id().getSuperclass()));
             }
         }
 
