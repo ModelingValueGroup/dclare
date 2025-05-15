0776b7fd5a6b43c53bb22d59ce85efdea87dfac2
WimBast <wim.bast@gmail.com>
2025-05-15 09:56:44 +0200

    not only lazy when pushNow on Mutable

--- src/main/java/org/modelingvalue/dclare/Mutable.java
+++ src/main/java/org/modelingvalue/dclare/Mutable.java
@@ -195,7 +195,7 @@ public interface Mutable extends TransactionClass {
             Set<Setable> containments = MutableClass.D_CONTAINMENTS.get(dClass);
             Set<Mutable> children = containments.flatMap(s -> s.<Mutable> getCollection(this)).asSet();
             children.forEach(m -> m.pushNow());
-            Set<Observer> nonDerivers = MutableClass.D_NON_DERIVERS.get(dClass).filter(d -> d.direction().isLazy()).asSet();
+            Set<Observer> nonDerivers = MutableClass.D_NON_DERIVERS.get(dClass).asSet();
             nonDerivers.forEach(o -> tx.runDeriver(this, null, o, 0));
             Set<Observed> observeds = MutableClass.D_PUSH_IF_PULL.get(dClass);
             observeds.forEach(o -> o.get(this));
