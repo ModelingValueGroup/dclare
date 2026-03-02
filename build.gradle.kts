5a657ef7156b5f2a19bb23f3e10027956119900f
Tom Brus <tombrus@gmail.com>
2026-03-02 12:49:45 +0100

    upgrade mvgplugin to 2.3.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "2.3.15"
+    id("org.modelingvalue.gradle.mvgplugin") version "2.3.18"
     idea
     eclipse
 }
