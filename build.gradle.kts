1c2d5bc8e8abf1617545a78d65abe6ca8e0ecf2b
Tom Brus <tombrus@gmail.com>
2026-03-05 14:45:35 +0100

    bump to new gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "2.3.19"
+    id("org.modelingvalue.gradle.mvgplugin") version "2.3.21"
     idea
     eclipse
 }
